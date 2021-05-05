41b2e5bd454004407875d0510e7c446ceac8e063
Dolphy Phan <dolphy.phanle@gmail.com>
2021-05-05 07:47:06 +0100

    Create cloud_push_prometheus.sh

---
+++ cloud_push_prometheus.sh
@@ -0,0 +1,30 @@
+## Dynamic Points
+# TODO.
+# Template variables
+
+export NOW="$(date +%s)"
+opts=$1
+data=$(curl -s -w "total:%{time_total} dns:%{time_namelookup} connect:%{time_connect} pretransfer:%{time_pretransfer} starttransfer:%{time_starttransfer} uploadspeed:%{speed_upload}" $1) 
+upload.speed=$(echo $data|cut -d ' ' -f 6| cut -d ':' -f 2)
+
+
+login="100383:eyJrIjoiMTU1MGM3OGQ3NjExNGY4MGZiYjYwNTU5ZjJlM2M2YmYyNjdhZWUwNCIsIm4iOiJkb2xwaHkiLCJpZCI6NDkyMTUwfQ=="
+url="https://prometheus-blocks-prod-us-central1.grafana.net/api/prom/push"
+# Curl command
+curl -X POST $url \
+-H "Content-Type: application/json" \
+-d @- << EOF
+{
+  "series": [
+    {
+      "metric": "upload.speed.mbps",
+      "points": [
+        [
+          "${NOW}",
+          "${upload.speed}"
+        ]
+      ]
+    }
+  ]
+}
+EOF
