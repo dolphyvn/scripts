e5dde5629145f1e8ec1619b6990abe4b6996aa53
Dolphy Phan <dolphy.phanle@gmail.com>
2021-05-05 07:50:52 +0100

    Update cloud_push_prometheus.sh

--- cloud_push_prometheus.sh
+++ cloud_push_prometheus.sh
@@ -5,13 +5,14 @@
 export NOW="$(date +%s)"
 opts=$1
 data=$(curl -s -w "total:%{time_total} dns:%{time_namelookup} connect:%{time_connect} pretransfer:%{time_pretransfer} starttransfer:%{time_starttransfer} uploadspeed:%{speed_upload}" $1) 
-upload.speed=$(echo $data|cut -d ' ' -f 6| cut -d ':' -f 2)
+upload_speed=$(echo $data|cut -d ' ' -f 6| cut -d ':' -f 2)
 
 
 login="100383:eyJrIjoiMTU1MGM3OGQ3NjExNGY4MGZiYjYwNTU5ZjJlM2M2YmYyNjdhZWUwNCIsIm4iOiJkb2xwaHkiLCJpZCI6NDkyMTUwfQ=="
 url="https://prometheus-blocks-prod-us-central1.grafana.net/api/prom/push"
 # Curl command
 curl -X POST $url \
+-u $login
 -H "Content-Type: application/json" \
 -d @- << EOF
 {
@@ -21,7 +22,7 @@ curl -X POST $url \
       "points": [
         [
           "${NOW}",
-          "${upload.speed}"
+          "${upload_speed}"
         ]
       ]
     }
