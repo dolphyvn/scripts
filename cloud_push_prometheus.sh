af6642a30600642b7af196b12ad924cdf1a1dbca
Dolphy Phan <dolphy.phanle@gmail.com>
2021-05-05 09:20:59 +0100

    Update cloud_push_prometheus.sh

--- cloud_push_prometheus.sh
+++ cloud_push_prometheus.sh
@@ -7,11 +7,12 @@ opts=$1
 data=$(curl -s -w "total:%{time_total} dns:%{time_namelookup} connect:%{time_connect} pretransfer:%{time_pretransfer} starttransfer:%{time_starttransfer} uploadspeed:%{speed_upload}" $1) 
 upload_speed=$(echo $data|cut -d ' ' -f 6| cut -d ':' -f 2)
 
-
-login="100383:eyJrIjoiMTU1MGM3OGQ3NjExNGY4MGZiYjYwNTU5ZjJlM2M2YmYyNjdhZWUwNCIsIm4iOiJkb2xwaHkiLCJpZCI6NDkyMTUwfQ=="
-url="https://prometheus-blocks-prod-us-central1.grafana.net/api/prom/push"
+graphite_login="100384:eyJrIjoiMTU1MGM3OGQ3NjExNGY4MGZiYjYwNTU5ZjJlM2M2YmYyNjdhZWUwNCIsIm4iOiJkb2xwaHkiLCJpZCI6NDkyMTUwfQ=="
+prom_login="100383:eyJrIjoiMTU1MGM3OGQ3NjExNGY4MGZiYjYwNTU5ZjJlM2M2YmYyNjdhZWUwNCIsIm4iOiJkb2xwaHkiLCJpZCI6NDkyMTUwfQ=="
+prom_url="https://prometheus-blocks-prod-us-central1.grafana.net/api/prom/push"
+graphite_url="https://graphite-us-central1.grafana.net/metrics"
 # Curl command
-curl -X POST $url \
+curl -X POST $graphite_url \
 -u $login
 -H "Content-Type: application/json" \
 -d @- << EOF
